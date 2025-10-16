640ceb3eedf47af061236606ed3cc8b7177249d1
sergiomitbo <smb0008@alu.medac.es>
2025-10-16 14:19:56 +0200

    Add files via upload

---
+++ EJercicio2.js
@@ -0,0 +1,12 @@
+for (let i = 1; i < 100; i++) {
+
+    if ( i % 4 == 0 && i % 7 == 0) {
+        console.log("FullStack");
+
+    } else if ( i % 4 == 0) {
+        console.log("FrontEnd");
+
+    } else if ( i % 7 == 0) {
+        console.log("BackEnd");
+}
+}
