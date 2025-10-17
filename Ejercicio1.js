2fe6bf67676f8293c4696ba299c8a1df3ea2d5fb
sergiomitbo <smb0008@alu.medac.es>
2025-10-17 17:26:04 +0200

    Delete Ejercicio1.js

--- Ejercicio1.js
+++
@@ -1,6 +0,0 @@
-let niveles = "5";
-for (let i=0; i < niveles; i++){
-    let espacios = " ".repeat(niveles - i - 1);
-    let asteriscos = "*".repeat(2 * i + 1);
-    console.log(espacios + asteriscos);
-}
